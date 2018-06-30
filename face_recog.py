94ab28ceb10b15845c49451cdbcc61aa710c2d76
Rishabh <rishabhrajshrm00@gmail.com>
2018-07-01 01:53:47 +0530

    Face_Detection

--- face_recog.py
+++ face_recog.py
@@ -2,32 +2,36 @@ import cv2
 import numpy as np
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.yml')
+
+
+font = cv2.FONT_HERSHEY_SIMPLEX 
+fontScale=1
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
-
-
 cap = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_SIMPLEX
-fontScale = 1
-fontColor = (255, 255, 255)
+recognizer.read("trainer/trainer.yml")
+id=0
+while 1:
+    ret,img = cap.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    faces=faceCascade.detectMultiScale(gray,1.2,5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        id,conf=recognizer.predict(gray[y:y+h,x:x+w])
+        print (id,conf)
+        if(conf>50):
+            if(id==1):
+                id="Rishabh"
+            elif(id==2):
+                id= "Pranjal"
+            elif(id==3):
+                id= "Himanshu"
 
-
-while True:
-    ret,im=cap.read()
-    gray= cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    faces=faceCascade.detectMultiScale(gray, 1.2,5)
-    for(x,y,w,h) in faces:
-        cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
-        Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
-        if(conf<50):
-            if(Id==1):
-                Id="Rishabh"
-            else:
-                Id="Unknown"
-        cv2.putText(im,str(Id), (x,y+h),font, fontScale,fontColor)
-    cv2.imshow('im',im) 
-    if cv2.waitKey(0):
+        else:
+            id="Unknown"
+        cv2.putText(img,str(id),(x,y+h),font, fontScale,(0,0,255))
+    cv2.imshow("Face",img)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
